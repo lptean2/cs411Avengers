0d0fc46df2cda014cb5a4a9708b6a5b9deb61035
Arif Topcu <09.arif@gmail.com>
2020-03-05 20:47:03 -0500

    react changes

--- ui-application/src/App.js
+++ ui-application/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Avengers 411 Project
-      </header>
-    </div>
-  );
+
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            const response = await window.fetch('http://avengers1.web.illinois.edu/cpi_api/items');
+            const json = await response.json();
+            console.log("json response", json);
+
+            if (Array.isArray(json)) {
+                setItems(json);
+            }
+        };
+
+        getData().then();
+    }, []);
+
+    return (
+        <div className="App">
+            <div>Database Items: </div>
+            <div>
+                {items.map(item => (
+                    <div key={item.ID}>{item.Name}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
