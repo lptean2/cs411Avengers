cbd7f41907fe41af4e0b80017914af6d11e6c552
Arif Topcu <09.arif@gmail.com>
2020-03-13 19:34:11 -0400

    ui hooks work

--- ui-application/src/components/Items.js
+++ ui-application/src/components/Items.js
@@ -1,33 +1,28 @@
-import React, {Component} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import Item from './Item';
-import {connect} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import { getItems } from '../actions/itemActions';
 
-class Items extends Component {
+const Items = () => {
+	const items = useSelector(state => state.items.items);
+	const dispatch = useDispatch();
+	const itemsList = useMemo(() => items.map(item => <Item key={item.ID} item={item}/>), [items]);
 
-	componentDidMount() {
-		this.props.getItems();
-	}
-	render(){
-		const itemsList = this.props.items.map(item => (
-			<Item key={item.ID} item={item}/>));
+	useEffect(() => {
+		dispatch(getItems());
+	}, []);
 
-		return(
-			<table>
-				<tbody>
-					<tr>
-						<th>Item ID</th>
-						<th>Item Name</th>
-					</tr>	
-					{itemsList}
-				</tbody>
-			</table>
-		)
-	}
+	return (
+		<table>
+			<tbody>
+			<tr>
+				<th>Item ID</th>
+				<th>Item Name</th>
+			</tr>
+			{itemsList}
+			</tbody>
+		</table>
+	);
 }
 
-const mapStateToProps = state => ({
-	items: state.items.items
-})
-
-export default connect(mapStateToProps, {getItems})(Items);
+export default Items;
